bf75688f8a7133b5d3250752fe81b363a3df7540
RogueAutomata <47926856+mepley1@users.noreply.github.com>
2023-10-08 20:58:01 -0500

    POST bad data for testing

--- project/test-post-bad-data.py
+++ project/test-post-bad-data.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+""" Testing script, just POSTs some random data to the endpoint """
+
 import requests
 import random
 import string
@@ -11,24 +13,18 @@ def get_random_string(length):
     result_str = ''.join(random.choice(letters) for i in range(length))
     return result_str
 
-#characters = string.ascii_letters + string.digits + string.punctuation
-#bad_data = ''.join(random.choice(characters) for i in range(16))
-
 bad_data = get_random_string(16)
-'''
-myobj = {
-    "bad data": bad_data
-    }
-'''
-myobj = bad_data
+
+#bad_data = {"bad data": bad_data}
+
 headers = {'Content-Type': 'application/json'}
 
-x = requests.post(url, data = myobj, headers = headers)
+x = requests.post(url, data = bad_data, headers = headers)
 
 # Print response
-#print(x.text)
-print(x.status_code)
 
+print(x.status_code)
+#print(x.text)
 #print(x.content)
 #print(x.encoding)
 #print(x.close())
@@ -37,13 +33,11 @@ print(x.status_code)
 #print(x.elapsed)
 #print(x.history)
 #print(x.is_redirect)
-'''
-x.iter_content()
-x.json()
-x.url
-x.request
-x.reason
-x.raise_for_status()
-x.ok
-x.links
-'''
+#x.iter_content()
+#x.json()
+#x.url
+#x.request
+#x.reason
+#x.raise_for_status()
+#x.ok
+#x.links

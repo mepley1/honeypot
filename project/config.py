838f850de981b126e6e5493a5c50d55a9eb3263e
RogueAutomata <47926856+mepley1@users.noreply.github.com>
2023-10-14 13:36:43 -0500

    test commit

--- project/config.py
+++ project/config.py
@@ -1,6 +1,6 @@
 """Make any extra flask app.config adjustments here to override defaults """
 
-import secrets
+#import secrets
 
 SECRET_KEY = secrets.token_hex() #Use a static value in production or else cookies won't stay valid
 PERMANENT_SESSION_LIFETIME = 86400

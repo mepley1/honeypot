c3d96b0b58b482f3fe77e5d7471003b8c755a050
RogueAutomata <47926856+mepley1@users.noreply.github.com>
2023-10-16 11:33:47 -0500

    Stop tracking config.py

--- project/config.py
+++
@@ -1,6 +0,0 @@
-"""Make any extra flask app.config adjustments here to override defaults """
-
-import secrets
-
-SECRET_KEY = secrets.token_hex() #Use a static value in production or else cookies won't stay valid
-PERMANENT_SESSION_LIFETIME = 86400

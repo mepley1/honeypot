25e9fbf64bd29aec43cd5cb53733913906794147
Mike <47926856+mepley1@users.noreply.github.com>
2023-10-18 09:09:48 -0500

    Example config

---
+++ project/config.py
@@ -0,0 +1,8 @@
+"""Make any extra flask app.config adjustments here to override defaults """
+
+import secrets
+
+SECRET_KEY = secrets.token_hex() #Use a static value in production or else cookies won't stay valid
+PERMANENT_SESSION_LIFETIME = 86400
+ABUSEIPDB = "12345678"
+DEBUG = False

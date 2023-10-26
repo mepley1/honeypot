253597775dfbcd1d5d0e603e84b26c4b3bb7f8de
RogueAutomata <47926856+mepley1@users.noreply.github.com>
2023-10-25 21:51:36 -0500

    More detections + misc 1. Add more strings to rules. 2. Add X-Forwarded-For to get_real_ip() function. 3. Add wordpress and misc_get_probe rules. 4. Formatting for readability. 5. Logging edit

--- project/auto_report.py
+++ project/auto_report.py
@@ -10,6 +10,8 @@ def get_real_ip():
     """ Get client's IP from behind Nginx. """
     if 'X-Real-Ip' in request.headers:
         real_ip = request.headers.get('X-Real-Ip')
+    elif 'X-Forwarded-For' in request.headers:
+        real_ip = request.headers.get('X-Forwarded-For')
     else:
         real_ip = request.remote_addr
     return real_ip
@@ -43,42 +45,90 @@ def submit_report(report_comment, report_categories):
 # BEGIN RULES
 
 def is_env_probe(request):
-    """ Returns True if path contains any of target strings. Only catch GET. """
+    """ Returns True if path contains any of target strings. Only catch GET for this rule.
+    Some environment/config probes I see often. """
     path = request.path
     method = request.method
-    ENV_PROBE_PATHS = ['.env', 'config', '/admin', '.git', 'backend', 'phpinfo', '/eval', 'echo.php', '/api', '/system/deviceinfo']
-    return any(target in path.lower() for target in ENV_PROBE_PATHS) if method == 'GET' else False
+    ENV_PROBE_METHODS = ['GET', 'HEAD']
+    ENV_PROBE_PATHS = [
+        '.env', #The big winner
+        'config', '/conf', '.conf',
+        '/admin',
+        '.git',
+        'backend',
+        'phpinfo',
+        '/eval',
+        'echo.php',
+        '/api',
+        '/system', '/deviceinfo', #seen as /system/deviceinfo
+        '/public',
+        '/src',
+        '/app',
+        '/www',
+        '/vendor',
+        '/laravel',
+        '/storage', '/protected', # seen as /storage/protected - Redlion RAS
+        '/library',
+        '/auth',
+        'database',
+        ]
+    if method in ENV_PROBE_METHODS:
+        return any(target in path.lower() for target in ENV_PROBE_PATHS)
+    return False
 
 def is_phpmyadmin_probe(request):
     """ Probing for PHPMyAdmin instances. """
     path = request.path
-    pma_probe_paths = ['phpmyadmin', '/mysql/', '/sqladmin/', '/mysqlmanager/', '/myadmin/']
-    return any(target in path.lower() for target in pma_probe_paths)
+    PMA_PROBE_PATHS = ['phpmyadmin', '/mysql/', '/sqladmin/', '/mysqlmanager/', '/myadmin/']
+    return any(target in path.lower() for target in PMA_PROBE_PATHS)
 
 def is_cgi_probe(request):
     path = request.path
-    cgi_probe_paths = ['.cgi', '.cc']
-    return any(target in path.lower() for target in cgi_probe_paths)
+    CGI_PROBE_PATHS = ['.cgi', '.cc', '/cgi-bin']
+    return any(target in path.lower() for target in CGI_PROBE_PATHS)
 
 def is_injection_attack(request):
     path_full = request.full_path
-    injection_signatures = [';sh', '|sh', '/tmp', 'file=', ';wget',]
+    INJECTION_SIGNATURES = [
+        ';sh',
+        '|sh',
+        '/tmp',
+        'file=',
+        ';wget',
+        ';chmod',
+        'cd+',
+    ]
     # Check for signatures in the path+query
-    return any(target in path_full.lower() for target in injection_signatures)
+    return any(target in path_full.lower() for target in INJECTION_SIGNATURES)
 
 def is_misc_software_probe(request):
     """ Misc software probes I see often. """
     path = request.path
-    misc_software_probe_paths = ['/adminer',
+    MISC_SOFTWARE_PROBE_PATHS = [
+        '/adminer',
         '/ReportServer',
         '/boaform', '/formLogin', #/boaform/admin/formLogin = Some OEM Fiber gear. Usually seen POSTing `username=admin&psd=Feefifofum`
         '/actuator', #/actuator/health - Sping Boot health check
         '/geoserver',
-        '/systembc',
+        '/systembc', #systembc malware, can move this to its own rule
         '/phpunit', '/eval-stdin.php', #phpunit framework
         '/webui', #Cisco ios xe - recent vuln being exploited - usually seen as /webui/logoutconfirm.html?logon_hash=1
+        '/mailchimp', # mailchimp probes
+        '/redlion', '/portal', #seen as /portal/redlion/
+        '/hudson', #Hudson CI
+        '/stalker_portal', #IPTV middleware
     ]
-    return any(target.lower() in path.lower() for target in misc_software_probe_paths)
+    return any(target.lower() in path.lower() for target in MISC_SOFTWARE_PROBE_PATHS)
+
+def is_wordpress_attack(request):
+    path = request.path
+    WORDPRESS_PATHS = [
+        '/wp-content',
+        '/wp-admin',
+        '/wp-login',
+        '/wp-upload',
+    ]
+    return any(target.lower() in path.lower() for target in WORDPRESS_PATHS)
 
 # More specific bots
 
@@ -91,8 +141,14 @@ def is_mirai_dvr(request):
     MIRAI_DVR_PATH = '/dvr/cmd'
     if request.method == 'POST' and path.lower() == MIRAI_DVR_PATH and request.content_type == 'application/x-www-form-urlencoded':
         form_data = request.form.get('data')
-        MIRAI_DVR_PAYLOADS = ['<DVR Platform="Hi3520">', '<SetConfiguration File="service.xml">', 
-            '&wget', '|sh', 'NTP Enable=', 'http://']
+        MIRAI_DVR_PAYLOADS = [
+            '<DVR Platform="Hi3520">',
+            '<SetConfiguration File="service.xml">',
+            '&wget',
+            '|sh',
+            'NTP Enable=',
+            'http://',
+            ]
         # If path==/dvr/cmd and any of the payload strings are there, it's definitely an attempt,
         # though not necessarily always Mirai- would have to check the file it downloads, but each one
         # that I've seen has been a Mirai loader.
@@ -131,6 +187,14 @@ def no_host_header(request):
     """ True if request contains no HOST header. """
     host_header = request.headers.get('Host')
     return host_header is None
+
+def is_misc_get_probe(request):
+    """ Any GET request that includes a query. """
+    MISC_PROBE_METHODS = ['GET', 'HEAD']
+    probe_args = request.args
+    if request.method in MISC_PROBE_METHODS and len(probe_args) > 0:
+        return True
+    return False
 
 # Some misc rules to help prevent false positives.
 # Don't be that dumbass admin who reports NTP servers etc.
@@ -179,10 +243,12 @@ def check_all_rules():
     # (rule function, log message, category code)
     rules = [
         (is_env_probe, 'Environment/config probe', ['21']),
+        (is_misc_get_probe, 'Misc GET probe', ['21']),
         (is_phpmyadmin_probe, 'PhpMyAdmin probe', ['21']),
-        (is_cgi_probe, 'CGI probe', ['21']),
+        (is_cgi_probe, 'CGI probe/attack', ['21']),
         (is_injection_attack, 'Command injection generic', ['21']),
         (is_misc_software_probe, 'Misc software probe', ['21']),
+        (is_wordpress_attack, 'Wordpress attack', ['21']),
         (is_mirai_dvr, 'HiSense DVR exploit', ['23','21']),
         (is_mirai_netgear, 'Netgear command injection exploit', ['23','21']),
         (is_androx, 'Detected AndroxGh0st', ['21']),
@@ -211,7 +277,7 @@ def check_all_rules():
         if current_app.config.get('ABUSEIPDB'):
             try:
                 reported = submit_report(report_comment, report_categories)
-                logging.info(f'Reported to AbuseIPDB. Matched {rules_matched} rules')
+                logging.info(f'Matched {rules_matched} rules. Reported to AbuseIPDB.')
             except requests.exceptions.ConnectionError as e:
                 reported = 0
                 logging.error(f'Connection error while submitting report: {str(e)}')
